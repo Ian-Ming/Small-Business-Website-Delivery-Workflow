3154e152d8bb5e7a2135bcb27e085692c0c44115
PrettyMing <pro.mingian@gmail.com>
2026-02-14 17:28:56 -0600

    fix: from google api to microsoft

--- src/blocks/AuthControlBlock.js
+++ src/blocks/AuthControlBlock.js
@@ -45,7 +45,7 @@ class AuthControlBlock extends HTMLElement {
             <span class="user-info">Hi, <strong>${this.user.userDetails}</strong></span>
             <a href="/.auth/logout?post_logout_redirect_uri=/" class="auth-link logout">Sign Out</a>
         ` : `
-                <a href="/.auth/login/google?post_login_redirect_uri=/admin.html">Client Login</a>
+                <a href="/.auth/login/aad?post_login_redirect_uri=/admin.html">Client Login</a>
         `}
         </div>
         `;
